e50dac9abcd0d35e96d0f60bc5bca9fecf40cd49
XuZuer <xxy_web@163.com>
2020-08-10 18:42:12 +0800

    。

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ import './registerServiceWorker'
 import router from './router'
 import VueSocketIO from 'vue-socket.io'
 
-if (window.location.hostname.indexOf("a.zhongtengwang.cn") != -1) {
-  Vue.use(new VueSocketIO({
-    debug: false,
-    connection: 'http://8.129.178.174:8086',
-  }))
-}else{
+if(process.env.NODE_ENV == "development"){
   Vue.use(new VueSocketIO({
     debug: true,
     connection: 'http://192.168.1.190:8086',
+  }))
+}else {
+  Vue.use(new VueSocketIO({
+    debug: false,
+    connection: 'http://8.129.178.174:8086',
   }))
 }
 
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  devServer: {
+    // port: 8080,
+    open: true,
+    autoOpenBrowser: true,
+  },
+  publicPath: './', // 基本路径
+  outputDir: 'dist', // 输出文件目录
+  productionSourceMap: false,
+};
